ddfb775dcbdec1dc381c592f708c828f7557884d
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-07-22 12:51:50 +0100

    refactor: db update

--- guestbook.sql
+++ guestbook.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2020 at 03:51 PM
+-- Generation Time: Jul 22, 2020 at 01:50 PM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.31
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,20 +29,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `guestbook` (
-  `id` int(4) NOT NULL,
-  `name` varchar(65) NOT NULL DEFAULT '',
-  `email` varchar(65) NOT NULL DEFAULT '',
-  `message` longtext NOT NULL,
-  `datetime` varchar(65) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `guestbook`
 --
 
-INSERT INTO `guestbook` (`id`, `name`, `email`, `message`, `datetime`) VALUES
-(9, 'Ope', 'ope@yahoo.com', 'I will like to book a room for a week from 6th to 13th of July. ', '20-07-02 03:24:14'),
-(10, 'Phillip Dallas', 'p_d@gmail.com', 'Can I get a room from your Room services.', '20-07-02 03:35:49');
+INSERT INTO `guestbook` (`id`, `name`, `email`, `message`, `created_at`) VALUES
+(1, 'Ereyomi', 'ereyomioluwaseyi@gmail.com', 'Hmmm just testing', '2020-07-21 23:00:00');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +61,7 @@ ALTER TABLE `guestbook`
 -- AUTO_INCREMENT for table `guestbook`
 --
 ALTER TABLE `guestbook`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
